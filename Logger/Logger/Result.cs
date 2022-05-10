bf8c77e7634e553f40f1c054056bc7d50169ad91
lena <llogvinovaaaa@gmail.com>
2022-05-10 19:53:02 +0200

    Logger

---
+++ Logger/Logger/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    internal class Result
+    {
+        public bool Status { get; set; }
+#pragma warning disable SA1201 // Elements should appear in the correct order
+        public Result()
+#pragma warning restore SA1201 // Elements should appear in the correct order
+        {
+        }
+
+        public Result(bool status)
+        {
+            Status = status;
+        }
+    }
+}
